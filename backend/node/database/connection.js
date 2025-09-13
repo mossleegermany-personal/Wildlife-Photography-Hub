636503743ad25f63f452789c31341f3eb3d3a2ef
mossleegermany-personal <mossleegermany@gmail.com>
2025-09-13 23:00:11 +0800

    Update connection.js

--- backend/node/database/connection.js
+++ backend/node/database/connection.js
@@ -1,7 +1,8 @@
 const { MongoClient, ObjectId } = require('mongodb');
 
 // Use environment variable if available, otherwise fallback to hardcoded URI
-const uri = process.env.MONGODB_URI || "mongodb+srv://mossleegermany_db_user:Mlxy6695@moses-personal.pvalfbk.mongodb.net/?retryWrites=true&w=majority&appName=Moses-Personal";
+//mongodb+srv://mossleegermany_db_user:<db_password>@moses-personal.pvalfbk.mongodb.net/?retryWrites=true&w=majority&appName=Moses-Personal
+const uri = "mongodb+srv://mossleegermany_db_user:Mlxy6695@moses-personal.pvalfbk.mongodb.net/?retryWrites=true&w=majority&appName=Moses-Personal";
 
 class DatabaseConnectivity {
     constructor(options = {}) {
